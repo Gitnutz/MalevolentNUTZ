83f2afce5c289a6848fbce4d7ff7f8f2ee7a4da5
aqil <ahmadaqilyusop@gmail.com>
2024-04-25 15:48:21 +0800

    Calc and classify BMI

--- Lab2.py
+++ Lab2.py
@@ -1,1 +1,14 @@
 print("ET0735 (DevOps for AIoT) - Lab 2 - Introduction to Python")
+def calculate_bmi(height, weight):     
+    print("Height = " + str(height)) 
+    print("Weight = " + str(weight)) 
+    bmi = weight/(height*height)
+    print("BMI = " + str(bmi)) 
+    if ( bmi < 18.5 ):
+        print("Dude is Under Weight")
+    if ( 18.5 < bmi < 25.0 ):
+        print("Dude is Normal Weight")
+    if ( bmi > 25.0 ):
+        print("Dude is Over Weight")
+calculate_bmi(weight=80, height=1.73)
+
