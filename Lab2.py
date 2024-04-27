cae8506b655067c4d7d7c28e1f22e852506d9d15
aqil <ahmadaqilyusop@gmail.com>
2024-04-28 06:08:04 +0800

    Lab 2

--- Lab2.py
+++ Lab2.py
@@ -1,14 +1,45 @@
+
+def display_main_menu(): 	      
+    print("Enter some numbers separated by commas (e.g. 5, 67, 32): ") 
+          
+def calc_average_temperature(): #Float  
+    temp=sum(y)/len(y)
+    return temp
+
+
+
+def get_user_input(): #List of Floats 
+    x = input()
+    num_list=x.split(",")
+    num_list=[float(x) for x in num_list]
+    return num_list
+
+def find_min_max_temperature(): #List of Floats in the format [min_temp, max_temp] 
+    min_temp = float('inf')
+    for num1 in y:
+        if (num1 < min_temp):
+            min_temp = int(num1)
+    max_temp = float('-inf')
+    for num2 in y:
+        if (num2 > max_temp):
+            max_temp = int(num2)
+    print("Min Temperature is:", min_temp, "Max Temperature is:", max_temp)
+    
+
+def sort_temperature(): #List of Floats sorted in ascending order 
+    print(sort_temperature)
+
+def calc_median_temperature(): #Float 
+    print(calc_median_temperature)
+
 print("ET0735 (DevOps for AIoT) - Lab 2 - Introduction to Python")
-def calculate_bmi(height, weight):     
-    print("Height = " + str(height)) 
-    print("Weight = " + str(weight)) 
-    bmi = weight/(height*height)
-    print("BMI = " + str(bmi)) 
-    if ( bmi < 18.5 ):
-        print("Dude is Under Weight")
-    if ( 18.5 < bmi < 25.0 ):
-        print("Dude is Normal Weight")
-    if ( bmi > 25.0 ):
-        print("Dude is Over Weight")
-calculate_bmi(weight=80, height=1.73)
+display_main_menu()
+y=get_user_input()
+print("List is:", y)
+calc_average_temperature()
+print("Average is:", calc_average_temperature())
+find_min_max_temperature()
+
+
+
 
